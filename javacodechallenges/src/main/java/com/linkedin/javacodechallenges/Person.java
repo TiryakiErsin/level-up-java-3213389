1984edced8a4d6355ce09d8c893946051d350c6e
Ersin Tiryaki <tiryakiersin28@gmail.com>
2024-04-13 18:44:56 +0000

    Implement Model a person

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/Person.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/Person.java
@@ -1,3 +1,23 @@
 public class Person {
+  private String firstName;
+  private String lastName;
+  private int age;
+
+  public Person(String firstName, String lastName, int age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
   
+  }
+
+  public void setAge(int age) {
+    this.age = age;
+  }
+
+  public int getAge() {
+    return this.age;
+  }
+
+  
+
 }
